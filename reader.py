0ca27c56eea32369da54a6376a94f18265b6b58b
JeevanIITH <cs20btech11047@iith.ac.in>
2022-03-01 22:59:06 +0530

    updated author table

--- reader.py
+++ reader.py
@@ -3,38 +3,73 @@ from itertools import count
 from turtle import title
 
 
-source_file= open('source.txt','r')
-output_file = open('output.txt','w')
-counter=3
-title_counter=0
-authors=0
-index=0
-count_=0
-while counter>0:
-    count_+=1
-    line = source_file.readline()
-    if not line:
-        counter-=1
-        continue
-    if line.startswith('#*'):
-        title_counter+=1
-        print(f"Title: {line.removeprefix('#*')}")
-    elif line.startswith('#@'):
-        authors+=line.count(',') + 1
-        print(f"Authours are {line.removeprefix('#@')}")
-    elif line.startswith('#t'):
-        print(f"year : {line.removeprefix('#t')}")
-    elif line.startswith('#c'):
-        print(f"Paper cited : {line.removeprefix('#c')}")
-    elif line.startswith('#index'):
-        print(f"ID : {line.removeprefix('#index')}")
-        index+=1
-    elif line.startswith('#!'):
-        print(f"Abstract : { line.removeprefix('#!')  }")
 
-print(f"No of lines is {count_} \n index is {index} \n Authors : {authors} \n Title : {title_counter} \n ")
+def read_all_details():
+    source_file= open('source.txt','r')
+    output_file = open('output.txt','w')
+    counter=3
+    title_counter=0
+    authors=0
+    index=0
+    count_=0
+    while counter>0:
+        count_+=1
+        line = source_file.readline()
+        if not line:
+            counter-=1
+            continue
+        if line.startswith('#*'):
+            title_counter+=1
+            print(f"Title: {line.removeprefix('#*')}")
+        elif line.startswith('#@'):
+            authors+=line.count(',') + 1
+            print(f"Authours are {line.removeprefix('#@')}")
+        elif line.startswith('#t'):
+            print(f"year : {line.removeprefix('#t')}")
+        elif line.startswith('#c'):
+            print(f"Paper cited : {line.removeprefix('#c')}")
+        elif line.startswith('#index'):
+            print(f"ID : {line.removeprefix('#index')}")
+            index+=1
+        elif line.startswith('#!'):
+            print(f"Abstract : { line.removeprefix('#!')  }")
 
-source_file.close()
-output_file.close()
+    print(f"No of lines is {count_} \n index is {index} \n Authors : {authors} \n Title : {title_counter} \n ")
+    source_file.close()
+    output_file.close()
     
+def read_authors():
+    try:
+        source_file= open('source.txt','r',encoding="utf8")
+        output_file = open('output.txt','w')
+    except:
+        print("Unable to open source.txt file")
+    counter=20
+    authors=0
+    Authors=[]
     
+    while counter>0:
+        Author=[]
+        line = source_file.readline()
+        if  line=='\n':
+            counter-=1
+            #print(f"counter : {counter}")
+            continue
+        if line.startswith('#@'):
+            authors+=line.count(',') + 1
+            #print(f"Authours are {line.removeprefix('#@')}")
+            line=line.removeprefix('#@')
+            Author=line.split(',')
+            Authors.extend(Author)       
+    source_file.close()
+    output_file.close()
+    return Authors
+
+
+if __name__=="_main_":
+    Authors=read_authors()
+    print(len(Authors))
+    for a in Authors:
+        if(a=='\n'):
+            print("new line\n")
+        print(a)
--- test.py
+++ test.py
@@ -4,7 +4,56 @@ db_user = 'postgres'
 db_pass = '1234'
 
 
+from ast import Try
+from sqlite3 import Cursor
 import psycopg2
-conn = psycopg2.connect(host = db_host , database = db_name , user = db_user , password = db_pass)
-conn.close()
+from reader import *
+
+def create_Author_table():
+    """ create a Author table """
+    sql_create_table = """CREATE TABLE IF NOT EXISTS Authors ( 
+                Author_ID SERIAL PRIMARY KEY,
+                Author_Name varchar(128) NOT NULL UNIQUE,
+                UNIQUE (Author_Name)
+                )    """
+    conn = psycopg2.connect(host = db_host , database = db_name , user = db_user , password = db_pass) 
+    cursor = conn.cursor()
+    cursor.execute(sql_create_table)
+    conn.commit()   
+    conn.close()  
+
+def Insert_into_Authors(names):
+    """Insert author name into authors table"""
+    insert="""INSERT INTO Authors(Author_Name)
+                VALUES(%s)
+                ON CONFLICT (Author_Name) DO NOTHING
+                RETURNING Author_ID """
+    conn = psycopg2.connect(host = db_host , database = db_name , user = db_user , password = db_pass) 
+    cursor = conn.cursor()
+    for name in names:
+        cursor.execute(insert,(name,))
+
+
+    """ #Author_ID = cursor.fetchone()[0]
+    #print(Author_ID)
+    try:
+      Author_ID = cursor.fetchone()[0]
+      if (Author_ID):
+        print(Author_ID)
+    except:
+        print('duplicate value') """
+     
+    conn.commit() 
+    conn.close()
+
+
+
+create_Author_table()
+
+Authors=read_authors()
+Insert_into_Authors(Authors)
+
+
+
+#conn.close()
 
